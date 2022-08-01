89d21b4a0391a858435823b3ecce0a459106d419
Marina Velikhanova <msa.940505@gmail.com>
2022-08-01 19:43:51 +0400

    Conditional statements and Data types Practical task

---
+++ learn_the_code.js
@@ -0,0 +1,19 @@
+let a = "30";
+let b = true;
+let c = 40;
+console.log (a + b);
+console.log (a + c);
+console.log (b + c);
+
+console.log (a * b);
+console.log (a * c);
+console.log (b * c);
+
+console.log (a / b);
+console.log (a / c);
+console.log (c / b);
+
+a = Number (a);
+b = String (b);
+c = Boolean (c);
+
